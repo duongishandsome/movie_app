6efe847b3e48fec0fef7539f5f10ddaa32ac8803
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-03-17 01:03:10 +0700

    footer-logo-container

--- client/src/components/common/Logo.js
+++ client/src/components/common/Logo.js
@@ -1,9 +1,16 @@
 import { Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
     const theme = useTheme();
     return (
-        <Typography>
+        <Typography
+            fontWeight="700"
+            fontSize="1.7rem"
+            component={Link}
+            to="/"
+            style={{ textDecoration: 'none', color: 'white' }}
+        >
             Movie<span style={{ color: theme.palette.primary.main }}>Chill</span>
         </Typography>
     );
--- client/src/components/layout/MainLayout.js
+++ client/src/components/layout/MainLayout.js
@@ -1,18 +1,27 @@
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import Footer from '../common/Footer';
 import GlobalLoading from '../common/GlobalLoading';
+import Topbar from '../common/Topbar';
 
 const MainLayout = () => {
     return (
         <>
-           {/* global loading  */}
+            {/* global loading  */}
             <GlobalLoading />
             {/* global loading */}
             <Box display="flex" minHeight="100vh">
+                {/* header */}
+                <Topbar />
+                {/* header */}
+
                 <Box component="main" flexGrow={1} overflow="hidden" minHeight="100vh">
                     <Outlet />
                 </Box>
             </Box>
+
+            {/* Footer */}
+            <Footer />
         </>
     );
 };
