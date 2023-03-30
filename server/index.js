17d4c028c9f3cb264747a84f9f4aede8dc140498
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-03-30 13:24:38 +0700

    fixx

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import http from 'http';
 import mongoose from 'mongoose';
 import 'dotenv/config';
-import routes from "./src/routes/index.js";
+import routes from './src/routes/index.js';
 
 const app = express();
 
@@ -13,11 +13,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/api/v1", routes)
+app.use('/api/v1', routes);
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+// Set Access-Control-Allow-Origin header
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+// Set other CORS headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 const connectDB = async () => {
     try {
