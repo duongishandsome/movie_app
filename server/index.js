58f343f16cbf77c5fbb3626b20e6fa3a56b9a3bf
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-03-30 16:54:43 +0700

    lai

--- server/index.js
+++ server/index.js
@@ -4,16 +4,15 @@ import cors from 'cors';
 import http from 'http';
 import mongoose from 'mongoose';
 import 'dotenv/config';
-import routes from "./src/routes/index.js";
+import routes from './src/routes/index.js';
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/api/v1", routes)
+app.use('/api/v1', routes);
 
 const PORT = process.env.PORT || 5000;
 
@@ -34,3 +33,4 @@ const connectDB = async () => {
 };
 
 connectDB();
+app.use(cors());
