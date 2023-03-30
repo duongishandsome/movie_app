5f883cd8e612cee6d2f6cd589ed2c8e76eec3bf8
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-03-30 15:48:51 +0700

    aaa

--- server/index.js
+++ server/index.js
@@ -7,29 +7,29 @@ import 'dotenv/config';
 import routes from './src/routes/index.js';
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api/v1', routes);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-mongoose
-    .connect(process.env.MONGODB_URL)
-    .then(() => {
-        console.log('Mongodb connected');
-        server.listen(port, () => {
-            console.log(`Server is listening on port ${port}`);
-        });
-    })
-    .catch((err) => {
-        console.log({ err });
-        process.exit(1);
-    });
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log('MongoDB connected!');
 
-//test
+        server.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+connectDB();
+app.use(cors());
