06e4799b61c76b84b770c8a86f81b3364ac670bd
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-03-30 13:58:33 +0700

    a

--- server/index.js
+++ server/index.js
@@ -8,22 +8,6 @@ import routes from './src/routes/index.js';
 
 const app = express();
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log('MongoDB connected!');
-
-        server.listen(PORT, () => {
-            console.log(`Server is listening on port ${PORT}`);
-        });
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-};
-
-connectDB();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,6 +15,21 @@ app.use(cookieParser());
 
 app.use('/api/v1', routes);
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log('Mongodb connected');
+        server.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log({ err });
+        process.exit(1);
+    });
+
+//test
